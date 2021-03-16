f0e0d50bb507aaeeb4ac62601199bcc2d799288a
WeslyG <weslyg22@gmail.com>
2021-03-16 22:05:06 +0500

    Подготовлен скрипт нагрузочного тестирования

---
+++ load_test/locustfile.py
@@ -0,0 +1,24 @@
+from locust import HttpUser, task, between
+
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2.5)
+
+    @task
+    def hello_world(self):
+        self.client.get("/", verify=False, headers={
+          "authorization": "Basic YWRtaW46YWRtaW4="
+        })
+        self.client.get("/_cat/indices", verify=False, headers={
+          "authorization": "Basic YWRtaW46YWRtaW4="
+        })
+
+    @task(3)
+    def view_items(self):
+        self.client.get("/_cluster/health", verify=False, headers={
+          "authorization": "Basic YWRtaW46YWRtaW4="
+        })
+
+    def on_start(self):
+        # self.client.post("/", )
+        self.client.verify = False
