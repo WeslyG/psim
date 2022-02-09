1714e73d285b33a7e13cb4d1682db426c5e28667
WeslyG Allan <weslyg22@gmail.com>
2022-02-10 04:10:43 +0500

    Работает парсилка конфига

---
+++ cpp/make.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+g++ pointInPlot.c
+chmod +x a.out
+./a.out
--- cpp/pointInPlot.c
+++ cpp/pointInPlot.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<stdbool.h>
+#include <stdbool.h>
+#include <string.h>
 
 #ifndef min
 #define min(a,b)            (((a) < (b)) ? (a) : (b))
@@ -98,39 +99,165 @@ bool isInside(struct Point polygon[], int n, struct Point p)
   return count & 1; // Same as (count%2 == 1)
 }
 
+  // char *token = strtok(rule_input_data, ",;", );
+  // int switcher = 0;
 
-bool test(double a)
-{
-  return a > 5 ? true : false;
-}
+  // while (token != NULL)
+  // {
+  //   struct Point tempPoint;
+  //   printf("token = %s\n", token);
+  //   if (switcher == 0) {
+  //     printf("%lf\n", atof(token));
+  //     tempPoint.x = atof(token);
+  //     switcher++;
+  //   } else {
 
-const char *const BoolToString(bool b)
-{
-  return b ? "true\n" : "false\n";
-}
-
-int getLength(struct Point p)
-{
-  return sizeof(p) / sizeof(p[0]);
-}
+  //     printf("%lf\n", atof(token));
+  //     tempPoint.y = atof(token);
+  //     switcher = 0;
+  //     room[i] = tempPoint;
+  //     i++;
+  //   }
+  //   token = strtok(NULL, ",;");
+  // }
 
 
 int main() {
-  printf("Enter point value:\n");
-  struct Point square_room[] = {{0, 0}, {10, 0}, {10, 10}, {0, 10}};
-  struct Point triangle_room[] = {{1.22, 0}, {4.9, 5.1}, {5, 0}};
-  struct Point circle_room[] = {{0, 0}, {-3, 3}, {0, 6}, {3, 9}, {6, 6}, {9, 3}, {6, 0}, {3, -3}};
-  // struct Point p = {1.22, -3};
+  // struct Point square_room[] = {{0, 0}, {10, 0}, {10, 10}, {0, 10}};
+  bool result;
+  char rule_input_data[]= "1,2.666;7.823,99.22;3.2,1.1;4.22,2.7;4.78,2.7;-142.583,999";
 
-  struct Point p;
-  scanf("%lf", &(p.x));
-  scanf("%lf", &(p.y));
-  printf("p.x = %lf\n", p.x);
-  printf("p.y = %lf\n", p.y);
+  struct Point current_point;
+  struct Point room[100] = {0};
 
-  int n = getLength(&square_room);
-  // int n = sizeof(square_room) / sizeof(square_room[0]);
-  printf(BoolToString(isInside(square_room, n, p)));
+  int len = strlen(rule_input_data);
+  int switcher = 1;
+  int state = 0;
+  int roomCount = 0;
+  struct Point tempPoint;
+
+  for (int i = 0; i <= len; i++) {
+    if (rule_input_data[i] == ',' || rule_input_data[i] == ';' || i == len) {
+      int q = i - state;
+      char tempStr[q + 1] = "";
+      // memcpy(tempStr, &rule_input_data[state], q);
+      strncpy(tempStr, &rule_input_data[state], q);
+
+      printf("tmpstr = %s\n", tempStr);
+      if (switcher % 2 != 0) {
+        tempPoint.x = atof(tempStr);
+      } else {
+        tempPoint.y = atof(tempStr);
+        // printf("---------\n");
+        // printf("%lf\n", tempPoint.x);
+        // printf("%lf\n", tempPoint.y);
+        // printf("---------\n");
+        printf("%i\n", roomCount);
+        // room = realloc(room, sizeof(tempPoint));
+        room[roomCount] = tempPoint;
+        roomCount++;
+        memset(&tempPoint, 0, sizeof(tempPoint));
+      }
+      switcher++;
+      state = i + 1;
+    }
+  }
+
+  printf("\n\n");
+  printf("!!!!!!!!!!!\n");
+
+  printf("%lf\n", room[0].x);
+  printf("%lf\n", room[0].y);
+
+  printf("--------------------\n");
+
+  printf("%lf\n", room[1].x);
+  printf("%lf\n", room[1].y);
+
+  printf("--------------------\n");
+
+  printf("%lf\n", room[2].x);
+  printf("%lf\n", room[2].y);
+
+  printf("--------------------\n");
+
+  printf("%lf\n", room[3].x);
+  printf("%lf\n", room[3].y);
+
+  printf("--------------------\n");
+
+  printf("%lf\n", room[4].x);
+  printf("%lf\n", room[4].y);
+
+  printf("--------------------\n");
+
+  printf("%lf\n", room[5].x);
+  printf("%lf\n", room[5].y);
+
+
+  // char *istr;
+  // istr = strtok(rule_input_data,sep);
+
+  // while (istr != NULL)
+  // {
+  //   printf("%s\n",istr);
+  //   // printf("%s\n",zzz);
+  //   char *my;
+  //   strcpy(istr, my);
+  //   // arr[iterator] = my;
+  //   // iterator++;
+  //   printf("%s\n", my);
+  //   istr = strtok(NULL,sep);
+  // }
+  // // printf("%i\n", sizeof(arr));
+
+  // Parse input_data and push
+  // rule_input_data.split('{'})
+  // room.push(input_data.map(i => i))
+
+  // int n = sizeof(room) / sizeof(room[0]);
+  // result = isInside(room, n, current_point);
+
+  // if (result == true) {
+  //   // printf("Hey you");
+  //   // os_strdup("true", lf->result);
+  //   // os_strdup("zzzz", lf->protocol);
+  // } else {
+  //   // os_strdup("false", lf->result);
+  //   // printf("another");
+  // }
+  // // return (NULL);
+
+  // struct Point square_room[] = {{0, 0}, {10, 0}, {10, 10}, {0, 10}};
+  // struct Point triangle_room[] = {{1.22, 33}, {4.9, 5.1}, {5, 0}};
+  // struct Point circle_room[] = {{0, 0}, {-3, 3}, {0, 6}, {3, 9}, {6, 6}, {9, 3}, {6, 0}, {3, -3}};
+
+  // scanf("%lf", &(p.x));
+  // scanf("%lf", &(p.y));
+
+  // struct Point p;
+  // scanf("%lf", &(p.x));
+  // scanf("%lf", &(p.y));
+  // printf("p.x = %lf\n", p.x);
+  // printf("p.y = %lf\n", p.y);
+
+  // printf(sizeof(square_room[0]));
+  // printf(sizeof(triangle_room[0]));
+  // printf(sizeof(circle_room[0]));
+
+  // int n = getLength(&square_room);
+
+
+  // printf("%i\n", sizeof(triangle_room));
+  // printf("%i\n", sizeof(circle_room[0]));
+  // printf("%i\n", sizeof(n));
+
+  // printf("%lf\n", square_room[1].y);
+  // printf("%lf\n", triangle_room[1].x);
+  // printf("%lf\n", circle_room[1].x);
+
+  // printf(BoolToString(isInside(square_room, n, p)));
+
   return 0;
 }
 
